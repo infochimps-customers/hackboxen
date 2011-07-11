595df16b33c904a3b320fca6485de25c45541ab8
Travis Dempsey <travis@infochimps.com>
2011-07-11 14:55:29 -0500

    endpoint and icss tasks now move multiple files, if applicable

--- lib/hackboxen/tasks/endpoint.rb
+++ lib/hackboxen/tasks/endpoint.rb
@@ -3,11 +3,10 @@ module HackBoxen
 
     desc "Copy an endpoint.rb file to the output code directory if the endpoint exists"
     task :endpoint => ['hb:create_required_paths'] do
-      endpoint = "#{WorkingConfig[:protocol]}_endpoint.rb"
-      srcfile  = File.join(path_to(:hb_engine), endpoint)
-      if File.exists? srcfile
+      endpoints = Dir[File.join(path_to(:hb_engine), '**/*_endpoint.rb')]
+      endpoints.each do |srcfile|
         HackBoxen.current_fs.mkpath path_to(:code_dir)
-        destfile = File.join(path_to(:code_dir), endpoint)
+        destfile = File.join(path_to(:code_dir), File.basename(srcfile))
         HackBoxen.current_fs.cp(srcfile, destfile)
       end
     end
--- lib/hackboxen/tasks/icss.rb
+++ lib/hackboxen/tasks/icss.rb
@@ -3,11 +3,13 @@ module HackBoxen
 
     desc "Copy the Icss file to the output directory in the filesystem specified in the WorkingConfig"
     task :icss => ['hb:create_required_paths'] do
-      icss_yaml   = File.join(path_to(:hb_config), "#{WorkingConfig[:protocol]}.icss.yaml")
-      if File.exists? icss_yaml
-        icss      = YAML.load(File.read icss_yaml)
-        icss_json = File.join(path_to(:fixd_dir), "#{WorkingConfig[:protocol]}.icss.json")
-        HackBoxen.current_fs.open(File.join(icss_json), 'w') { |f| f.puts icss.to_json }
+      icss_yamls    = Dir[File.join(path_to(:hb_config), '**/*.icss.yaml')]
+      icss_yamls.each do |icss_yaml|
+        icss_hsh    = YAML.load(File.read(icss_yaml))
+        if icss_hsh
+          icss_json = File.join(path_to(:fixd_dir), "#{icss_hsh['protocol']}.icss.json")
+          HackBoxen.current_fs.open(File.join(icss_json), 'w') { |f| f.puts icss_hsh.to_json }
+        end
       end
     end
 
