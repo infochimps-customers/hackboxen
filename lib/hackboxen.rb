ec8cdfe09fb921c189c9c01b808f9af9cb1e8134
Philip (flip) Kromer <flip@infochimps.org>
2011-08-14 11:24:29 -0500

    moved config loading to utils/load_config.rb. It should call WorkingConfig.resolve!, but left that commented out til it's verified in production

--- lib/hackboxen.rb
+++ lib/hackboxen.rb
@@ -37,11 +37,4 @@ require 'hackboxen/utils'
 ::HackBoxen.extend Rake::DSL
 require 'hackboxen/tasks'
 
-machine_cfg = "/etc/hackbox/hackbox.yaml"
-user_cfg    = File.join(ENV['HOME'], '.hackbox/hackbox.yaml')
-hackbox_cfg = File.join(HackBoxen.find_root_dir, 'config/config.yaml')
-
-WorkingConfig.read machine_cfg if File.exists? machine_cfg
-WorkingConfig.read user_cfg    if File.exists? user_cfg
-WorkingConfig.read hackbox_cfg if File.exists? hackbox_cfg
-
+require 'hackboxen/utils/load_config'
