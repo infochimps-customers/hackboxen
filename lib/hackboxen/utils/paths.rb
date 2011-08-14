a7667f332b5cb9c5ffc04b9a2ff76ac4bdddef7f
Philip (flip) Kromer <flip@infochimps.org>
2011-08-14 11:22:53 -0500

    made it so I can hack in and fix paths before they're forced to a value

--- lib/hackboxen/utils/paths.rb
+++ lib/hackboxen/utils/paths.rb
@@ -1,15 +1,15 @@
 require 'pathname'
 
 module HackBoxen
-
   module Paths
+    module_function
 
     def paths
       @paths ||= default_paths
     end
 
     def path_to *pathsegs
-      path = Pathname.new path_to_helper(*pathsegs)
+      path = Pathname.new HackBoxen::Paths.path_to_helper(*pathsegs)
       path.absolute? ? File.expand_path(path) : path.to_s
     end
 
@@ -17,14 +17,14 @@ module HackBoxen
       {
         # root directories
         :home        => ENV['HOME'],
-        :data_root   => HackBoxen.dataroot,
-        :code_root   => HackBoxen.coderoot,
+        :data_root   => :get_data_root,
+        :code_root   => :get_code_root,
         # local hackbox directories
-        :hb_current  => [:code_root, HackBoxen.current, HackBoxen.name],
+        :hb_current  => [:code_root, :get_hb_current, :hb_name],
         :hb_engine   => [:hb_current, 'engine'],
         :hb_config   => [:hb_current, 'config'],
         # output directories
-        :hb_dataroot => [:data_root, HackBoxen.current],
+        :hb_dataroot => [:data_root, :get_hb_current],
         :ripd_dir    => [:hb_dataroot, 'ripd'],
         :rawd_dir    => [:hb_dataroot, 'rawd'],
         :fixd_dir    => [:hb_dataroot, 'fixd'],
@@ -33,16 +33,19 @@ module HackBoxen
         :env_dir     => [:hb_dataroot, 'env'],
         :data_dir    => [:fixd_dir, 'data'],
         :code_dir    => [:fixd_dir, 'code']
-    }
-
+      }
     end
 
     private
-    def path_to_helper *pathsegs
+    def self.path_to_helper *pathsegs
       expanded = pathsegs.flatten.compact.map do |pathseg|
         case
         when pathseg.is_a?(Symbol) && paths.include?(pathseg) then path_to(paths[pathseg])
-        when pathseg.is_a?(Symbol)                            then raise "No path expansion set for #{pathseg.inspect}"
+        when pathseg == :get_hb_current then HackBoxen.current
+        when pathseg == :hb_name        then HackBoxen.name
+        when pathseg == :get_data_root  then HackBoxen.dataroot
+        when pathseg == :get_code_root  then HackBoxen.coderoot
+        when pathseg.is_a?(Symbol)      then raise "No path expansion set for #{pathseg.inspect}"
         else pathseg
         end
       end
@@ -50,5 +53,4 @@ module HackBoxen
     end
 
   end
-
 end
