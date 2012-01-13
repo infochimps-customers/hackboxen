32037bab85ab024ec1375c063af4dd37e9f9237e
Travis Dempsey <dempsey.travis@gmail.com>
2012-01-13 17:01:08 -0600

    removing old files

--- lib/hackboxen.rb
+++ lib/hackboxen.rb
@@ -5,36 +5,11 @@ require 'swineherd'
 require 'configliere'
 require 'json'
 
-# # require 'hackboxen/utils' causes the following:
-#
-# WorkingConfig  = Configliere::Param.new
-# WorkingConfig.use :commandline, :config_file
-#
-# module Hackboxen
-#   autoload :ConfigValidator, 'hackboxen/utils/config_validator'
-#   autoload :Paths,           'hackboxen/utils/paths'
-#   # require 'pathname'
-#   autoload :Logging,         'hackboxen/utils/logging'
-#   # require 'log4r'
-# end
-#
-# require 'pathname' # from utils/paths, force-invoked below
-#
-require 'hackboxen/utils'
-
-# # require 'hackboxen/tasks' causes the following:
-#
-# require 'hackboxen/tasks/init'
-# include Rake::DSL
-# require 'hackboxen/tasks/icss'
-# require 'hackboxen/tasks/mini'
-# # require 'swineherd'
-# # require 'configliere'
-# require 'hackboxen/tasks/endpoint'
-#
-# include HackBoxen::Paths
-#
-::HackBoxen.extend Rake::DSL
-require 'hackboxen/tasks'
-
 require 'hackboxen/utils/load_config'
+
+require 'hackboxen/tasks/init'
+require 'hackboxen/tasks/icss'
+
+include HackBoxen::Paths
+
+::HackBoxen.extend Rake::DSL
